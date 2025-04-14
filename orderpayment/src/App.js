9d127eefa923592a111f10bda1f5aa8c2c1c499b
Bhanuka Fernando <bhanuka.fernando@arimaclanka.com>
2025-04-14 23:52:00 +0530

    created a sample frontend

---
+++ orderpayment/src/App.js
@@ -0,0 +1,12 @@
+import './Component/Header/nav'
+import Navbar from './Component/Header/nav';
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ orderpayment/src/Component/Header/nav.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './nav.css'; // Import the CSS file
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="https://cdn-icons-png.flaticon.com/512/1046/1046784.png" alt="Logo" />
+        <span>FoodieExpress</span>
+      </div>
+
+      <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">Restaurants</a>
+        <a href="#">Contact</a>
+        <a href="#">Deliveries</a>
+        <a href="#">My Cart</a>
+        
+        <button className="login-btn">Login</button>
+      </div>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? 'rotate1' : ''}`}></div>
+        <div className={`bar ${isOpen ? 'hide' : ''}`}></div>
+        <div className={`bar ${isOpen ? 'rotate2' : ''}`}></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
